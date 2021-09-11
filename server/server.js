a645548a4d39919d74e9d484313bbcad09baa110
Hamzah <82611798+brobro10000@users.noreply.github.com>
2021-09-11 10:10:22 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -22,10 +22,22 @@ async function start() {
 }
 start()
 
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+
+
+
 db.once('open', () => {
   // integrate our Apollo server with the Express application as middleware
- server.applyMiddleware({app})
- app.listen(PORT, () => {
+  server.applyMiddleware({ app })
+  app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     // log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
